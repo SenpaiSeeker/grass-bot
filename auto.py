1359ebb15417fb67da6f3f8eb6be15e3e80a6da6
Anime Soul <x.15.18.19.15.4.9.11.9.14.x@gmail.com>
2024-11-23 10:08:52 +0700

    Create auto.py

---
+++ auto.py
@@ -0,0 +1,46 @@
+import os, asyncio, aiohttp 
+
+from loguru import logger
+
+
+async def fetch_proxies(api_url):
+    """Mengambil daftar proxy dari API."""
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api_url) as response:
+                if response.status == 200:
+                    proxies = (await response.text()).strip().splitlines()
+                    logger.info(f"Fetched {len(proxies)} proxies from API.")
+                    return proxies
+                else:
+                    logger.warning(f"Failed to fetch proxies. Status code: {response.status}")
+                    return []
+    except Exception as e:
+        logger.error(f"Error fetching proxies: {e}")
+        return []
+      
+
+def save_proxies(proxy_file, proxies):
+    """Menyimpan proxy ke file."""
+    try:
+        os.system(f"rm -rf {proxy_file}")
+        with open(proxy_file, 'w') as file:
+            file.writelines([proxy + '\n' for proxy in proxies])
+        logger.info(f"Saved {len(proxies)} proxies to {proxy_file}.")
+    except Exception as e:
+        logger.error(f"Error saving proxies: {e}")
+
+
+async def main():
+    proxy_api_url = "https://api.proxyscrape.com/v4/free-proxy-list/get?request=display_proxies&proxy_format=protocolipport&format=text"
+    while True:
+        proxies = await fetch_proxies(proxy_api_url)
+        save_proxies('proxy.txt', proxies)
+        await asyncio.sleep(60 * 60)
+
+
+  if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.info("Program terminated by user.")
